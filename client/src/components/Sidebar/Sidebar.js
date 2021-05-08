51a6ed5a072b5d640c52a1a1b8162ddf7a68f0b7
NguyenCong176708 <conghoahoa1cnh@gmail.com>
2021-05-09 01:05:58 +0700

    ok

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Sidebar.css';
 import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
-            <Button className='button-fix' >Nhập Lại</Button>
-            <Button className='button-fix' >Danh sách nghi ngờ</Button>
+            <Button className='button-fix' ><Link to="/" className='link-to'>Nhập Liệu</Link></Button>
+            <Button className='button-fix' ><Link to="/search" className='link-to'>Danh sách nghi ngờ</Link></Button>
         </div>
     );
 }
--- client/src/containers/SearchPatient.js
+++ client/src/containers/SearchPatient.js
@@ -1,21 +1,11 @@
 import React from "react";
-import Sidebar from './Sidebar/Sidebar';
-import Footer from './Footer/Footer';
-import Navigation from './Nav/Navigation';
+import Sidebar from '../components/Sidebar/Sidebar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Search from './Search/Search';
+import Search from '../components/Search/Search';
 
 function SearchPatient() {
-  // const [data, setData] = React.useState(null);
-
-  // React.useEffect(() => {
-  //   fetch("/api")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data.message));
-  // }, []);
-
   return (
     <div className='page'>
       
