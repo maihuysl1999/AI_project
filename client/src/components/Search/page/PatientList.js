1ec104927b3fdce09c6d0c9d3e9c6f3780cd1b53
maihuy <maihuysl1999@gmail.com>
2021-05-12 22:30:44 +0700

    table search

--- client/src/components/Search/page/PatientList.js
+++ client/src/components/Search/page/PatientList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Table from 'react-bootstrap/Table';
 
 PatientList.propTypes = {
     posts: PropTypes.array,
@@ -13,13 +14,29 @@ function PatientList(props) {
     const { posts } = props;
 
     return (
-        <ul className="post-list">
-            <h1>danh sach</h1>
-            {posts.map(post => (
-                <li key={post.id}>{post.name}-{post.address}-{post.idno}--{post.planeid}</li>
-            ))
-            }
-        </ul>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Idno</th>
+                    <th>Planeid</th>
+                </tr>
+            </thead>
+            <tbody>
+                {posts.map(post => (
+                    <tr>
+                        <td>{post.id}</td>
+                        <td>{post.name}</td>
+                        <td>{post.address}</td>
+                        <td>{post.idno}</td>
+                        <td>{post.planeid}</td>
+                    </tr>
+                ))
+                }
+            </tbody>
+        </Table>
     );
 }
 
